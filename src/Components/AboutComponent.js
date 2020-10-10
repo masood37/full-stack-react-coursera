c33e78aa62d6484fab6e23920c7fcf875f2c54e9
Masood Khan <khanmasood37@gmail.com>
2020-10-10 13:52:03 +0530

    Controlled Forms

--- src/Components/AboutComponent.js
+++ src/Components/AboutComponent.js
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Card,
-  CardBody,
-  CardHeader,
-  Media
-} from "reactstrap";
+import {Breadcrumb,BreadcrumbItem,Card,CardBody,CardHeader,Media} from "reactstrap";
 import { Link } from "react-router-dom";
 
 function About(props) {
