f78374b99a1fa376453c67cd892b9ce977642366
Masood Khan <khanmasood37@gmail.com>
2020-11-16 19:32:04 +0530

    Assignment 4

--- src/Components/AboutComponent.js
+++ src/Components/AboutComponent.js
@@ -1,13 +1,17 @@
 import React from "react";
 import {Breadcrumb,BreadcrumbItem,Card,CardBody,CardHeader,Media} from "reactstrap";
 import { Link } from "react-router-dom";
+import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
+import { Fade, Stagger } from "react-animation-components";
+
 
 function About(props) {
   function RenderLeader({ leader }) {
     return (
       <Media className="mt-5">
         <Media left className="mr-5">
-          <Media object src={leader.image} alt={leader.name} />
+          <Media object src={baseUrl + leader.image} alt={leader.name} />
         </Media>
         <Media body>
           <Media heading>{leader.name}</Media>
@@ -18,6 +22,22 @@ function About(props) {
     );
   }
 
+  function RenderContent({ leaders, isLoading, errMess }) {
+    if (isLoading) {
+      return <Loading />;
+    } else if (errMess) {
+      return <h4>{errMess}</h4>;
+    } else
+      return (
+        <Stagger in>
+          {props.leaders.map(leader => (
+            <Fade in key={leader.id}>
+              <RenderLeader key={leader.id} leader={leader} />
+            </Fade>
+          ))}
+        </Stagger>
+      );
+  }
   return (
     <div className="container">
       <div className="row">
@@ -95,9 +115,11 @@ function About(props) {
         </div>
         <div className="col-12">
           <Media list>
-            {props.leaders.map(leader => (
-              <RenderLeader key={leader.id} leader={leader} />
-            ))}
+            <RenderContent
+              leaders={props.leader}
+              isLoading={props.leaderLoading}
+              errMess={props.leaderErrMess}
+            />
           </Media>
         </div>
       </div>
